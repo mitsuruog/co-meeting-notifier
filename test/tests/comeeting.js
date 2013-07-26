2fd13f9bfeb1d3e6687ebb8a79204fff4dadc297
Mitsuru Ogawa <mitsuru.ogawa.jp@gmail.com>
2013-07-27 00:23:19 +0900

    Add first test case

---
+++ test/tests/comeeting.js
@@ -0,0 +1,16 @@
+describe('Tests comeeting.js', function() {
+
+	beforeEach(function() {
+
+
+	});
+
+
+	it('認証用のURLが取得できること', function() {
+		var authorizationUrl = 'https://www.co-meeting.com/oauth/authorize' +
+				'?response_type=code&client_id=consumer_key&redirect_uri=chrome-extension://app_id/callback.html';
+		expect(comeetingNotifier.getAuthorizationUrl()).to.be(authorizationUrl);
+	});
+
+
+});
